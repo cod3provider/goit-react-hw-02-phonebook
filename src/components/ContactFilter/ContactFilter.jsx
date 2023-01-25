23eabf8f200438ed842bad905bb9eb304824af1e
Ilya Bondarchuk <cod3provider@gmail.com>
2023-01-25 18:53:13 +0200

    small fixes

--- src/components/ContactFilter/ContactFilter.jsx
+++ src/components/ContactFilter/ContactFilter.jsx
@@ -2,13 +2,14 @@ import PropTypes from 'prop-types';
 
 import s from './ContactFilter.module.css';
 
-const ContactFilter = ({ filter, handleChange }) => {
+const ContactFilter = ({ value, handleChange }) => {
   return (
     <div className={s.filter}>
       <label >Find contacts by name
         <input
           name="filter"
           type="text"
+          value={value}
           onChange={handleChange}
           className={s.input}
         />
@@ -20,6 +21,6 @@ const ContactFilter = ({ filter, handleChange }) => {
 export default ContactFilter;
 
 ContactFilter.propTypes = {
-  filter: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
   handleChange: PropTypes.func.isRequired,
 };
